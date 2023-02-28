cf443068074568c5133209e76975cf3eb717a9c8
BeastLe9enD <beastle9end@outlook.de>
2023-02-28 23:51:53 +0100

    🐛 Fix merge conflict

--- crates/tort_math/src/lib.rs
+++ crates/tort_math/src/lib.rs
@@ -4,8 +4,6 @@
 pub use bevy_math::*;
 #[cfg(target_arch = "spirv")]
 pub use spirv_std::glam::*;
-<<<<<<< HEAD
-=======
 
 mod aabb;
 
@@ -15,4 +13,3 @@ pub fn dequantize_unorm(value: u32, n: u32) -> f32 {
     let scale = ((1i32 << n) - 1i32) as f32;
     value as f32 / scale
 }
->>>>>>> b4c5a3a7bd457f283f9813d24b6c3364190628fa
